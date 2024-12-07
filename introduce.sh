4fa897f19ded768447856e202dcd8acc488f2aa4
Ubuntu <ubuntu@ip-172-31-93-126.ec2.internal>
2024-12-07 05:55:53 +0000

    Add introduction script with personal details

---
+++ introduce.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+name="Gulkaiyr"
+age=32
+favourite_language="Linux"
+hobby="singing"
+dream="becoming a Devops engeneer"
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning , $name like to 
