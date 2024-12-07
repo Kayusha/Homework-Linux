acbd2ce287f3ce5821d423f09dd72cbbc1d906d3
Ubuntu <ubuntu@ip-172-31-93-126.ec2.internal>
2024-12-07 18:41:27 +0000

    added new lines

--- introduce.sh
+++ introduce.sh
@@ -7,4 +7,8 @@ favourite_language="Linux"
 hobby="listening to the music"
 dream="becoming a Devops engeneer"
 
-echo "Hi, my name is $name, and I'm $age years old. Every morning i love to  $hobby. My dream is $dream "  
+echo "Hi, my name is $name, and I'm $age years old. Every morning i love to  $hobby. My dream is $dream "
+
+echo "When I'm not busy working toward $dream, you'll find me $hobby to relax."
+
+echo "That's a little about me, $name — a $age-year-old who loves $favorite_language, enjoys $hobby, and dreams of $dream>!"
